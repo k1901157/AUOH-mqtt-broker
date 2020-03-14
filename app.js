9e95fe7a730a80875eda1ecaf3211def42afba45
Osama Alshriefy <osama.alshriefy@seamk.fi>
2020-03-14 11:10:23 +0200

    heroku

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const aedes = require('aedes')();
 const httpServer = require('http').createServer();
 const ws = require('websocket-stream');
-const port = process.env.PORT || 8081;
+const port = process.env.PORT || 442;
 
 ws.createServer({
     server: httpServer
